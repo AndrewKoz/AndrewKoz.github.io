722c5f5d07b5db0459d92d78570fd7ff314ca161
Andrew Kozlov <52545407+AndrewKoz@users.noreply.github.com>
2020-10-23 19:49:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const year = 2020;
+let input = window.prompt("Какой сейчас год?");
+if (input == 2020) {
+    alert("Правильно!");
+    alert("Вы такой умный!");
+} else {
+    alert("Не угадали! Сейчас 2020.");
+}
