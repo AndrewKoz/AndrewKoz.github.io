78c3e82168655e6fa547f10174145aed566f951b
Andrew Kozlov <skywalkerandrewk@gmail.com>
2020-10-24 15:37:38 +0300

    Try to fix import error.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
